f04e7e007b024b3d9b6f03315c1dd286178181f5
hr-shuvo <h.r.shuvo9@gmail.com>
2026-05-28 11:19:16 +0600

    moved

--- app/(boilerplate)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { MarketingShell } from "@/components/marketing-shell";
-
-export default function BoilerplateLayout({ children }: { children: ReactNode }) {
-    return <MarketingShell>{children}</MarketingShell>;
-}
